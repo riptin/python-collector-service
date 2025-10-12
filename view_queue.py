cb0dc9151f0e6a517b89d3db65d591cf045127b9
Sandro <s.martashvili@gmail.com>
2025-10-12 15:16:25 +0400

    View redis queue

---
+++ view_queue.py
@@ -0,0 +1,10 @@
+import redis
+import json
+from config import REDIS_HOST, REDIS_PORT, REDIS_PASSWORD, REDIS_QUEUE_NAME
+
+redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD, decode_responses=True)
+
+items = redis_client.lrange(REDIS_QUEUE_NAME, 0, -1)
+for item in items:
+    data = json.loads(item)
+    print(data)
