d047a4d008e7a2fc561a6634ebd67be5b8dc313a
Sandro <s.martashvili@gmail.com>
2025-10-12 14:39:03 +0400

    Add msqt user credentials

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 # import install_packages
 # install_packages.check_packages()
 
-from config import BROKER, PORT
+from config import BROKER, PORT, USERNAME, PASSWORD
 import redis_callbacks
 import paho.mqtt.client as mqtt
 
 client = mqtt.Client()
 client.on_connect = redis_callbacks.on_connect
 client.on_message = redis_callbacks.on_message
+client.username_pw_set(USERNAME, PASSWORD)
 
 client.connect(BROKER, int(PORT), 60)
 client.loop_forever()
--- config.py
+++ config.py
@@ -3,9 +3,11 @@ import os
 
 load_dotenv()
 
-BROKER = os.getenv('BROKER_ADDRESS', 'localhost')
-PORT = os.getenv('BROKER_PORT', 1883)
-TOPIC = os.getenv('BROKER_TOPIC', 'sensors/#')
+BROKER = os.getenv('MSQT_ADDRESS', 'localhost')
+PORT = os.getenv('MSQT_PORT', 1883)
+USERNAME = os.getenv('MSQT_USERNAME', '')
+PASSWORD = os.getenv('MSQT_PASSWORD', '')
+TOPIC = os.getenv('MSQT_TOPIC', 'sensors/#')
 REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
 REDIS_PORT = os.getenv('REDIS_PORT', 6379)
 REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', '')
