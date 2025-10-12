4d374340eb0f700fbaf5de4ff42b225434cb2460
Sandro <s.martashvili@gmail.com>
2025-10-12 14:08:21 +0400

    Convert port value to int

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ client = mqtt.Client()
 client.on_connect = redis_callbacks.on_connect
 client.on_message = redis_callbacks.on_message
 
-client.connect(BROKER, PORT, 60)
+client.connect(BROKER, int(PORT), 60)
 client.loop_forever()
