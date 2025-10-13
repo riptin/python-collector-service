407dedb7bda1ec824797758d00afe35327cdb96b
Sandro <s.martashvili@gmail.com>
2025-10-13 23:41:05 +0400

    Update topic structure

--- sample_sensor.py
+++ sample_sensor.py
@@ -8,8 +8,9 @@ AUTH = {
 }
 
 while True:
-    # topic structure: sensors/<facility_id>/<sensor_type_id>/<sensor_id>
+    # topic structure: sensors/<facility_id>/<gateway_id>/<sensor_type_id>/<sensor_id>
     facility_id = random.randint(1, 100)
+    gateway_id = random.randint(1, 20)
     sensor_type_id = random.randint(1, 4)
     sensor_id = random.randint(1, 100)
     temp = round(random.uniform(20, 30), 2)
@@ -17,7 +18,7 @@ while True:
     print(f"Temp: {temp}°C")
 
     try:
-        publish.single(f"sensors/{facility_id}/{sensor_type_id}/{sensor_id}", str(temp), hostname=BROKER, auth=AUTH)
+        publish.single(f"sensors/{facility_id}/{gateway_id}/{sensor_type_id}/{sensor_id}", str(temp), hostname=BROKER, auth=AUTH)
     except Exception as e:
         print("Publish failed:", e)
     time.sleep(10)
