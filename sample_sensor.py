d407ff5cbc3aca2a79593ace4f93bfff11b3b613
Sandro <s.martashvili@gmail.com>
2025-10-13 16:49:45 +0400

    Generate random topi names

--- sample_sensor.py
+++ sample_sensor.py
@@ -2,18 +2,24 @@ import time, random
 import paho.mqtt.publish as publish
 from config import BROKER, USERNAME, PASSWORD
 
-TOPIC = "sensors/test"
 AUTH = {
     'username': USERNAME,
     'password': PASSWORD
 }
 
+sensors = ['thermometer', 'infrared', 'pyrometer']
+
 while True:
+    # topic structure: sensors/<facility_id>/<sensor_type>/<sensor_id>
+    facility_id = random.randint(1, 100)
+    sensor_type = random.choice(sensors)
+    sensor_id = random.randint(1, 100)
     temp = round(random.uniform(20, 30), 2)
+
     print(f"Temp: {temp}°C")
 
     try:
-        publish.single(TOPIC, str(temp), hostname=BROKER, auth=AUTH)
+        publish.single(f"sensors/{facility_id}/{sensor_type}/{sensor_id}", str(temp), hostname=BROKER, auth=AUTH)
     except Exception as e:
         print("Publish failed:", e)
     time.sleep(10)
